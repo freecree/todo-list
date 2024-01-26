84ec69c900d341c9931fddaecbecd6f70424d87f
freecree <bohdansidorov@gmail.com>
2024-01-26 15:26:10 +0200

    deploy: prepare for deploying

--- src/components/ui/checkbox/Checkbox.tsx
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './checkbox.scss';
 
 interface CheckboxProps {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todo-list/',
   resolve: {
     alias: {
       '@variables': '/src/scss/variables.scss',
